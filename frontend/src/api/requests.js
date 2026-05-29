999f51ef584fe5883680144cd9bfe3418b57afed
Sean Smith <smith.sean1998@gmail.com>
2026-05-29 20:56:13 +0100

    refactor login function to accept credentials and use post request

--- frontend/src/api/requests.js
+++ frontend/src/api/requests.js
@@ -1,9 +1,9 @@
-import {instance} from './index';
+import {get, post} from './index';
 
-export const login = async () => {
+export const login = async (credentials) => {
   try {
-    const response = await instance.get('/login');
-    return response.data;
+    const response = await post('/login', credentials);
+    return response;
   } catch (error) {
     console.error('Error fetching login:', error);
     throw error;
